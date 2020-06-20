fe9678b9e45ea00c90ce7b82c3b66b053fcf1cf9
Paul Götze <paul.christoph.goetze@gmail.com>
2020-06-20 16:56:57 +0200

    Run beginn/endless range tests only for respective Ruby versions
    >= 2.6: endless range
    >= 2.7: beginnless range

--- spec/code_breaker/parser/ranges_spec.rb
+++ spec/code_breaker/parser/ranges_spec.rb
@@ -18,35 +18,39 @@ describe CodeBreaker::Parser do
       end
     end
 
-    context 'for a beginless inclusive range' do
-      it 'returns a Hash with key :irange and the upper bounding type' do
-      input = '..1.2'
-      output = { irange: [NilClass, Float]}
-      expect(input).to be_parsed_as output
+    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
+      context 'for a beginless inclusive range' do
+        it 'returns a Hash with key :irange and the upper bounding type' do
+        input = '..1.2'
+        output = { irange: [NilClass, Float]}
+        expect(input).to be_parsed_as output
+        end
+      end
+
+      context 'for a beginless exclusive range' do
+        it 'returns a Hash with key :erange and the upper bounding type' do
+        input = '...1.2'
+        output = { erange: [NilClass, Float]}
+        expect(input).to be_parsed_as output
+        end
       end
     end
 
-    context 'for a beginless exclusive range' do
-      it 'returns a Hash with key :erange and the upper bounding type' do
-      input = '...1.2'
-      output = { erange: [NilClass, Float]}
-      expect(input).to be_parsed_as output
+    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.6.0')
+      context 'for a endless inclusive range' do
+        it 'returns a Hash with key :irange and the lower bounding type' do
+        input = '1.2..'
+        output = { irange: [Float, NilClass]}
+        expect(input).to be_parsed_as output
+        end
       end
-    end
 
-    context 'for a endless inclusive range' do
-      it 'returns a Hash with key :irange and the lower bounding type' do
-      input = '1.2..'
-      output = { irange: [Float, NilClass]}
-      expect(input).to be_parsed_as output
-      end
-    end
-
-    context 'for a endless exclusive range' do
-      it 'returns a Hash with key :erange and the lower bounding type' do
-      input = '1.2...'
-      output = { erange: [Float, NilClass]}
-      expect(input).to be_parsed_as output
+      context 'for a endless exclusive range' do
+        it 'returns a Hash with key :erange and the lower bounding type' do
+        input = '1.2...'
+        output = { erange: [Float, NilClass]}
+        expect(input).to be_parsed_as output
+        end
       end
     end
   end
